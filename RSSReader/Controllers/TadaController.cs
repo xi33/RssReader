52cd7ac682f96217926c1cf9fba8af65165da087
Pieux Xi <pieuxxi@gmail.com>
2012-06-17 21:51:41 +0800

    initial and complete

---
+++ RSSReader/Controllers/TadaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RSSReader.Controllers
+{
+    public class TadaController : Controller
+    {
+        //
+        // GET: /Tada/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ RSSReader/Models/Rss.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Xml.Linq;
+
+namespace RSSReader.Models
+{
+    public class Rss
+    {
+        public string Link { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+
+    public static class RssReader
+    {
+        private static string _blogRssUrl = "http://pieux.diandian.com/rss";
+
+        public static IEnumerable<Rss> GetRssFeed()
+        {
+            XDocument feedXml = XDocument.Load(_blogRssUrl);
+            var feeds = from feed in feedXml.Descendants("item")
+                        select new Rss
+                        {
+                            Title = feed.Element("title").Value,
+                            Link = feed.Element("link").Value,
+                            Description = Regex.Match(feed.Element("description").Value, @"^.{1,180}\b(?<!\s)").Value
+                        };
+            return feeds;
+        }
+    }
+}
